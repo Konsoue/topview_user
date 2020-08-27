6d1c8b4fb5ca958a5c622db1ce4fdf8e805b491b
remy-cpu <3062857934@qq.com>
2020-08-27 19:46:47 +0800

    小黑框提示

--- js/login.js
+++ js/login.js
@@ -1,6 +1,4 @@
 $(function () {
-  let baseUrl = 'https://server1.backend.topviewclub.cn';
-
   $('[name=username]').on('focus', function () {
     $(this).siblings().eq(0).css('top', '-80%');
   })
@@ -18,16 +16,27 @@ $(function () {
     }
   })
 
+   //显示提示
+   function showTips(tips) {
+    var tipBox = document.querySelector('.tip_box');
+    tipBox.style.display = 'block';
+    tipBox.innerText = tips;
+    var timer = setTimeout(function () {
+      tipBox.style.display = 'none';
+      clearTimeout(timer);
+    }, 1000);
+  }
+
   $('.login_btn').on('click', login);
   function login() {
     const username = $('[name=username]').val();
     const password = $('[name=password]').val();
     if (username == '') {
-      alert('用户名不可为空');
+      showTips('用户名不可为空');
       return false;
     }
     if (password == '') {
-      alert('密码不可为空');
+      showTips('密码不可为空');
       return false;
     }
     $.ajax({
@@ -39,7 +48,6 @@ $(function () {
         'Content-Type': 'application/json'
       },
       success: function (data) {
-        console.log(JSON.stringify(data));
         if (data.code == 200) {
           $.cookie("token", data.data.token ,{
             expires: data.data.expireTime/60/60/24
