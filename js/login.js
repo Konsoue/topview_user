5c271d69787691f1d7a429fb9f2e704eaca31fe7
Konsoue <2329371328@qq.com>
2020-08-29 19:36:32 +0800

    add change-pwd

--- js/login.js
+++ js/login.js
@@ -1,5 +1,124 @@
 $(function () {
-  let baseUrl = 'https://server1.backend.topviewclub.cn';
+
+  /* 
+  *@author: 思贤
+  *@function: 登录成功后，页面样式变化
+  *@params
+  */
+ function styleChange() {
+   $('.login').fadeOut(1000,'linear');
+   window.onresize = function() {
+     if (document.body.clientWidth > 765) {
+       $('.sx-container').addClass('hasIn');
+     }else {
+       $('.sx-container').removeClass('hasIn');
+     }
+   }
+ }
+  /* 
+  *@author：思贤
+  *@function：欢迎语的获取数据
+  *@params：status是不同阶段对应是数字 0 ~ 5
+  */
+  function welcome(status) {
+    $.ajax({
+      url: "js/welcome.json",
+      async: true,
+      success: (data)=>{
+        whichStatus(status, data);
+      },
+      error: ()=> {
+        console.log("请求失败");
+      }
+    })
+  }
+
+  /* 
+  *@author: 思贤
+  *@function: 手机尺寸下，点击nav切换页面
+  *@params
+  */
+  function toggleNav() {
+    $('.nav li').eq(1).data('num',0);
+    $('.nav li').eq(2).data('num',1);
+    console.log($('.nav li').eq(1));
+    $('.nav li').click(function(){
+      console.log($(this).data('num'));
+      if ($(this).data('num')) {
+        $('.queue').show();
+        $('.user').hide();
+      }else {
+        $('.user').show();
+        $('.queue').hide();
+      }
+    })
+  }
+
+  /* 
+  *@author: 思贤
+  *@function: 登录成功后，获取个人信息，并添加到页面上 
+  *@params
+  */
+  function addUserMsg() {
+    $.ajax({
+      type: 'get',
+      url: '/api/student/getStudentInfo',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Authorization': getToken()
+      },
+      success: function(data) {
+        console.log(data);
+        if(data.success) {
+          $('#name').text(data.data.name);
+          $('#stuNum').text(data.data.studentNumber);
+          $('#groupName').text(data.data.groupName);
+          welcome(data.data.status);
+          if(data.data.groupNumber) {
+            $('#groupNumber').text(data.data.groupNumber).css('display',);
+          }
+        }
+      },
+      error: function() {
+        console.log("请求失败");
+      }
+    })
+  }
+  /* 
+  *@author: 思贤
+  *@function: 处理不同考核状态的信息
+  *@params: status是不同阶段对应是数字 0 ~ 5  ; data是欢迎语的数据
+  */
+  function whichStatus(status, data) {
+    console.log('data',data);
+    switch(status) {
+        case 0: $('#status').text('笔试阶段');$('.welcome').text(data[status]);break;
+        case 1: $('#status').text('面试阶段');$('.welcome').text(data[status]);break;
+        case 2: $('#status').text('已面试');$('.welcome').text(data[status]);break;
+        case 3: $('#status').text('面试通过');$('.welcome').text(data[status]);break;
+        case 4: $('#status').text('考核通过');$('.welcome').text(data[status]);break;
+        default: $('#status').text('考核未通过');$('.welcome').text(data[status]);break;
+    }
+  }
+
+  /*@author: 
+   *@function: 修改密码
+   *@params
+  */
+  function changePwd() {
+    $('.change-pwd').on('click',function() {
+      $('.user-pwd').show();
+      
+    })
+    $('#new-pwd').on('click',function() {
+      let pwd_data = {
+        oldpassword: $('#oldPassword').val(),
+        newpassword: $('#newPassword').val()
+      }
+      
+    })
+  }  
+  changePwd();
 
   $('[name=username]').on('focus', function () {
     $(this).siblings().eq(0).css('top', '-80%');
@@ -18,6 +137,12 @@ $(function () {
     }
   })
   $('.login_btn').on('click', login);
+
+  /* 
+   *@author: 敏仪
+   *@function: 登录后，执行的功能
+   *@params
+  */
   function login() {
     const username = $('[name=username]').val();
     const password = $('[name=password]').val();
@@ -30,7 +155,7 @@ $(function () {
       return false;
     }
     $.ajax({
-      url: baseUrl + '/api/login',
+      url:  '/api/login',
       type: 'POST',
       data: JSON.stringify({ 'username': username, 'password': password }),
       dataType: 'json',
@@ -38,10 +163,14 @@ $(function () {
         'Content-Type': 'application/json'
       },
       success: function (data) {
-        console.log(JSON.stringify(data));
+        // console.log(JSON.stringify(data));
         if (data.code == 200) {
           $.cookie("token", data.data.token);
-          $(".login").fadeOut(100);
+          styleChange();
+          getQueueMsg();
+          addUserMsg();
+          toggleNav();
+          changePwd();
         }
       },
       error: function () {
@@ -50,5 +179,6 @@ $(function () {
     })
   }
 
-
+ 
 })
+
--- js/sx-queue.js
+++ js/sx-queue.js
@@ -1,7 +1,77 @@
-$.ajax({
-    url: "js/welcome.json",
-    async: true,
-    success: (data)=>{
-        $('.welcome').text(data.wel);
+/* 
+ *@author：思贤
+ *@function：提取token的值
+ *@params
+*/
+function getToken() {
+  let cookie = document.cookie;
+  let reg = /;/;
+  let arr = cookie.split(reg);
+//   console.log('cookie: ',cookie.replace("%20"," "));
+  let which = null;
+  arr.every((current,index)=>{
+    if (current.slice(0, 5) === 'token') {
+      which = index;
+      return false;
     }
+    return true;
+  })
+//   console.log(arr[which].replace('%20',' ').substr(6));
+  return arr[which].replace('%20',' ').substr(6);
+}
+
+
+
+
+
+/*@author: 思贤
+ *@funtion:获取排队信息
+*/
+function getQueueMsg() {
+  $.ajax({
+    url:  '/api/student/getStatus',
+    type: 'GET',
+    dataType: 'json',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': getToken()
+    },
+    success: function (data) {
+      console.log(data);
+      if (data.code === 200) {
+        // 判断是否是排队时间
+        if (data.data.message === 0) {
+          $(".queue-msg").text('现在还不是排队时间哟');    
+        }
+        // 判断是否已经排队
+        if (data.data.message === 1) {
+          $(".queue-msg").text('尚未排队，点击按钮排队哟');
+        }else {
+          $(".queue-msg").text('现在你排在' + data.count + '个');
+        }
+      }
+    },
+    error: function () {
+      console.log('请求出错！');
+    }
+  })
+}
+
+
+// 开始排队
+$('#queue').click(()=>{
+  $.ajax({
+    url:  '/api/student/queueUp',
+    type: 'get',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Authorization': getToken()
+    },
+    success: function(data) {
+      $('.queue-msg').text(data.message);
+    },
+    error: function() {
+      console.log("请求出错");
+    }
+  })
 })
